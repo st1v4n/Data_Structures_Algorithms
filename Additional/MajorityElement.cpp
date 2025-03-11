65ac707630ea27a6ae68002407513c2369d4e0a1
st1v4n <149948320+st1v4n@users.noreply.github.com>
2025-03-11 18:44:22 +0200

    Create MajorityElement.cpp

---
+++ Additional/MajorityElement.cpp
@@ -0,0 +1,18 @@
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+Leetcode 169
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> umap;
+        for(int i=0;i<nums.size();++i){
+            umap[nums[i]]++;
+            if(umap[nums[i]] > nums.size()/2)return nums[i];
+        }
+        return -1;
+    }
+};
