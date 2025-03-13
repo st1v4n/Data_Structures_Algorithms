4d522fde5bd2d10e3da010950f68cbf503b0c1b4
st1v4n <149948320+st1v4n@users.noreply.github.com>
2025-03-13 15:09:56 +0200

    Create IntersectOfTwoArrays2.cpp

---
+++ Additional/IntersectOfTwoArrays2.cpp
@@ -0,0 +1,24 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+Leetcode 350
+*/
+
+class Solution {
+public:
+
+    int arr[1001]{0};
+
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        for(int i=0;i<nums1.size();++i){
+            arr[nums1[i]]++;
+        }
+        vector<int> result;
+        for(int i=0;i<nums2.size();++i){
+            if(arr[nums2[i]]){
+                result.push_back(nums2[i]);
+                arr[nums2[i]]--;
+            }
+        }
+        return result;
+    }
+};
