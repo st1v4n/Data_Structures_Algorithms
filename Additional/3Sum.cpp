6c56acbcad8111a39cf4b0ec809bff151b464bcd
st1v4n <149948320+st1v4n@users.noreply.github.com>
2025-03-23 17:22:17 +0200

    Create 3Sum.cpp

---
+++ Additional/3Sum.cpp
@@ -0,0 +1,37 @@
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+Leetcode 15
+*/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    while(left < right){
+                        if(nums[left] == nums[left-1])left++;
+                        else break;
+                    }
+                }
+            }
+        }
+        return result;        
+    }
+};
