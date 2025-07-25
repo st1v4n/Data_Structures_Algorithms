41527e479b36101ea005c9110a45328d1c53d193
st1v4n <149948320+st1v4n@users.noreply.github.com>
2025-07-25 11:46:49 +0300

    Create CountCompleteTreeNodes.java

---
+++ Additional/CountCompleteTreeNodes.java
@@ -0,0 +1,40 @@
+/*
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+Leetcode 222
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int total = 0;
+
+    public void dfs(TreeNode curr){
+        if(curr == null)return;
+        dfs(curr.left);
+        dfs(curr.right);
+        total++;
+    }
+
+    public int countNodes(TreeNode root) {
+        dfs(root);
+        return total;
+    }
+}
