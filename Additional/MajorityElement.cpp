2f74ea7060b4940bf12a682e327f7c8fe62fa73e
st1v4n <s.aleksandrov37@abv.bg>
2025-07-27 11:22:23 +0300

    Test-branch

--- Additional/MajorityElement.cpp
+++ Additional/MajorityElement.cpp
@@ -1,5 +1,5 @@
 /*
-TEST
+TEST2
 Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
